006efaaa408c8ffd227c6b9744d1c498997f71f9
Hyominn <kris42@163.com>
2020-07-31 23:26:08 +0800

    feat(startup): hot-reload

--- dotnetcore.mvc.demo/Startup.cs
+++ dotnetcore.mvc.demo/Startup.cs
@@ -23,7 +23,11 @@ namespace dotnetcore.mvc.demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            // Note that run time is not include in this list by default
+            // Add the Nuget package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
+            services
+                .AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
