076b95f700bf70aefe351e5e5f841c28fba0c473
Hyominn <kris42@163.com>
2020-08-03 23:18:23 +0800

    feat(girl): add details

---
+++ dotnetcore.mvc.demo/Views/Girl/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetcore.mvc.demo.Views.Girl
+{
+    public class Details : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
