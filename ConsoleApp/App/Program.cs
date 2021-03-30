74df716fcdb5ee0b302618839826feabaec693f8
Kris Nie <krisnie@qq.com>
2021-03-30 10:54:17 +0800

    feat: Added ConsoleApp

---
+++ ConsoleApp/App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Services;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UnbelievableClass.UnbelievableMethod();
+        }
+    }
+}
---
+++ ConsoleApp/Services/ResponseResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class ResponseResult
+    {
+        /// <summary>
+        /// success or not
+        /// </summary>
+        public bool isSuccess;
+
+        /// <summary>
+        /// timestamp
+        /// </summary>
+        public long beginTimeStamp = DateTime.Now.ToFileTime();
+
+        /// <summary>
+        /// Time cost
+        /// </summary>
+        public int timeCost;
+
+        /// <summary>
+        /// error message
+        /// </summary>
+        public string errMsg;
+
+        /// <summary>
+        /// data
+        /// </summary>
+        public object data;
+
+    }
+}
