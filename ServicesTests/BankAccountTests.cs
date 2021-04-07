d4758a9882d8adeffd3b679e3a9c60631dccede6
Kris Nie <krisnie@qq.com>
2021-04-07 23:10:59 +0800

    feat(UnitTest): Unit Test Demo

---
+++ ServicesTests/BankAccountTests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using Services;
+namespace ServicesTests
+{
+    public class BankAccountTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
