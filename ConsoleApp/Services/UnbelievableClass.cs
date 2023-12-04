c40840da6a34f0dfdf4e3270806b960e1f007a32
Kris Nie <krisnie@qq.com>
2023-12-04 17:45:36 +0800

    feat(ALL): Upgrade to .net 8

--- ConsoleApp/Services/UnbelievableClass.cs
+++ ConsoleApp/Services/UnbelievableClass.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Globalization;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Collections;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Services.Finance;
-using static System.FormattableString;
 
 namespace Services
 {
@@ -20,8 +15,9 @@ namespace Services
     {
         public static void UnbelievableMethod()
         {
+            var i = "AOE".IndexOf("A", 0, StringComparison.OrdinalIgnoreCase);
+            Console.WriteLine(i);
         }
-
 
         #region Dependency Injection Demo
 
@@ -33,37 +29,6 @@ namespace Services
             ba.Credit(5.77);
             ba.Debit(11.22);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
-        }
-
-        #endregion
-
-        #region Database Access Demo
-
-        private static DataSet GetDataSet()
-        {
-            var ds = new DataSet();
-            try
-            {
-                var builder = new SqlConnectionStringBuilder
-                {
-                    DataSource = "LOCALHOST", UserID = "sa", Password = "123456Zz",
-                    InitialCatalog = "CSI10"
-                };
-
-                using var connection = new SqlConnection(builder.ConnectionString);
-                connection.Open();
-
-                var sql = "SELECT TOP 10 * FROM item_mst;";
-
-                using var dataAdapter = new SqlDataAdapter(sql, connection);
-                dataAdapter.Fill(ds);
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            return ds;
         }
 
         #endregion
@@ -108,6 +73,96 @@ namespace Services
         private static void PrintBuffer(CircularBuffer<int> buffer)
         {
             Console.WriteLine($"{{{string.Join(",", buffer.ToArray())}}}");
+        }
+
+        #endregion
+
+        #region Asynchronous Programming Demo
+
+        private static async Task<bool[]> GetAllSite()
+        {
+            var uris = new List<string>
+            {
+                "https://www.google.com/",
+                "https://github.com/KrisNie",
+                "https://developer.mozilla.org/en-US/",
+                "https://leetcode.cn/"
+            };
+
+            var tasks = uris.Select(HttpGetAsync).ToList();
+            var results = await Task.WhenAll(tasks).ConfigureAwait(false);
+            foreach (var result in results) Console.WriteLine(result);
+            return results;
+        }
+
+        private static async Task<bool> HttpGetAsync(string requestUri)
+        {
+            try
+            {
+                var client = new HttpClient();
+                Console.WriteLine(requestUri);
+                using var cancellationToken = new CancellationTokenSource(5000);
+                using var response = await client.GetAsync(requestUri, cancellationToken.Token)
+                    .ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+                // var responseBody =
+                //     await response.Content.ReadAsStringAsync(cancellationToken.Token)
+                //         .ConfigureAwait(false);
+                // Console.WriteLine(responseBody);
+                Console.WriteLine("OK");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        private static void Upload()
+        {
+            AWS.S3.S3Bucket.ListBucketRootContentsAsync().GetAwaiter().GetResult();
+
+            var files = new Dictionary<string, byte[]>
+            {
+                {
+                    "L3-Apr22_2022_2_9.txt",
+                    File.ReadAllBytes("C:\\Development\\installs\\L3-Apr22_2022_2_9.txt")
+                },
+                {
+                    "L3-Dec22_2022_9_20 1.txt",
+                    File.ReadAllBytes(
+                        "C:\\Development\\installs\\L3-Dec22_2022_9_20 1.txt")
+                }
+            };
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var results = UploadAsync(files).GetAwaiter().GetResult();
+            watch.Stop();
+            var elapsed = watch.ElapsedMilliseconds;
+            Console.WriteLine($"All tasks cost {elapsed} ms");
+
+            if (results.Count > 0)
+            {
+                var message = string.Join(
+                    " ",
+                    results);
+                Console.WriteLine($"ExportTenantData.Run(): Post files failed. {message}");
+            }
+        }
+
+        private static async Task<HashSet<string>> UploadAsync(
+            Dictionary<string, byte[]> files)
+        {
+            var tasks = (from key in files.Keys
+                         let memoryStream =
+                             new MemoryStream(files[key])
+                         select AWS.S3.S3Bucket.UploadAsync(memoryStream, key))
+                .ToList();
+
+            var results = await Task.WhenAll(tasks).ConfigureAwait(false);
+
+            return results.Where(x => x.IsSuccess == false).Select(f => f.Message).ToHashSet();
         }
 
         #endregion
