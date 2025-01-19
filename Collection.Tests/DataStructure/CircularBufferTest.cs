fc1152c7e0f5ca5cc47a7caaf77417cc8128e92d
Kris Nie <krisnie@sent.com>
2025-01-19 20:34:09 +0800

    Feat: Implement the Circular Buffer with the unit test.

---
+++ Collection.Tests/DataStructure/CircularBufferTest.cs
@@ -0,0 +1,293 @@
+﻿using Collections.DataStructure.Lists;
+
+namespace Collection.Tests.DataStructure;
+
+public class CircularBufferTest
+{
+    [Fact]
+    public void SetsFixedLength()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(3);
+        // Act
+        var length = circularBuffer.Capacity;
+        // Assert
+        Assert.Equal(3, length);
+    }
+
+    [Fact]
+    public void ChecksIsEmptyProperty()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(4);
+        // Act
+        // Assert
+        Assert.True(circularBuffer.IsEmpty);
+    }
+
+    [Fact]
+    public void ChecksIsFilledProperty()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(3, false);
+        // Act
+        circularBuffer.Add(1);
+        circularBuffer.Add(2);
+        circularBuffer.Add(3);
+        // Assert
+        Assert.True(circularBuffer.IsFull);
+    }
+
+    [Fact]
+    public void InitializesWithDefaultLengthOf10()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>();
+        // Act
+        var length = circularBuffer.Capacity;
+        // Assert
+        Assert.Equal(10, length);
+    }
+
+    [Fact]
+    public void ThrowsArgumentOutOfRangeExceptionForLengthLessThanOne()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () =>
+            {
+                var _ = new CircularBuffer<byte>(0);
+            });
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () =>
+            {
+                var _ = new CircularBuffer<byte>(-2);
+            });
+    }
+
+    [Fact]
+    public void ThrowsCircularBufferFullExceptionWhenInsertingInFullBuffer()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Throws<CircularBufferFullException>(
+            () =>
+            {
+                var _ = new CircularBuffer<byte>(3, false)
+                {
+                    1,
+                    2,
+                    3,
+                    4
+                };
+            });
+    }
+
+    [Fact]
+    public void WritesAndReadsValue()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(4)
+        {
+            13,
+            43,
+            23,
+            2
+        };
+        // Act
+        var result1 = circularBuffer.Pop();
+        var result2 = circularBuffer.Pop();
+        var result3 = circularBuffer.Pop();
+        var result4 = circularBuffer.Pop();
+        var result5 = circularBuffer.Pop();
+        var result6 = circularBuffer.Pop();
+        var result7 = circularBuffer.Pop();
+        var result8 = circularBuffer.Pop();
+        // Assert
+        Assert.Equal(13, result1);
+        Assert.Equal(43, result2);
+        Assert.Equal(23, result3);
+        Assert.Equal(2, result4);
+        Assert.Equal(0, result5);
+        Assert.Equal(0, result6);
+        Assert.Equal(0, result7);
+        Assert.Equal(0, result8);
+    }
+
+    [Fact]
+    public void TestingCantOverrideFunctionality()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(3, false)
+        {
+            3,
+            34,
+            24
+        };
+
+        // Act
+        // Assert
+        Assert.Throws<CircularBufferFullException>(() => { circularBuffer.Add(2); });
+        Assert.Equal(3, circularBuffer.Pop());
+        Assert.Equal(34, circularBuffer.Pop());
+        Assert.Equal(24, circularBuffer.Pop());
+    }
+
+    [Fact]
+    public void TestingWritingAndReadingSimultaneouslyWithoutOverriding()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(3, false)
+        {
+            3,
+            34,
+            24
+        };
+        // Act
+        circularBuffer.Pop();
+        circularBuffer.Pop();
+        circularBuffer.Add(4);
+        circularBuffer.Add(14);
+        // Assert
+        Assert.Equal(24, circularBuffer.Pop());
+        Assert.Equal(4, circularBuffer.Pop());
+        Assert.Equal(14, circularBuffer.Pop());
+    }
+
+    [Fact]
+    public void TestingICollectionImplementation()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(3, false)
+        {
+            3,
+            34,
+            24
+        };
+        var array = new byte[3];
+        // Act
+        circularBuffer.CopyTo(array, 0);
+        circularBuffer.Clear();
+        // Assert
+        Assert.Equal(3, array[0]);
+        Assert.Equal(34, array[1]);
+        Assert.Equal(24, array[2]);
+        Assert.Equal(0, circularBuffer.Pop());
+        Assert.Equal(0, circularBuffer.Pop());
+        Assert.Equal(0, circularBuffer.Pop());
+        Assert.Empty(circularBuffer);
+    }
+
+    [Fact]
+    public void WhenRemoveItemNotExists_ThenRemoveNothing()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(5, false);
+        circularBuffer.Add(3);
+        circularBuffer.Add(34);
+        circularBuffer.Add(24);
+        circularBuffer.Add(31);
+        circularBuffer.Add(14);
+        // Act
+        circularBuffer.Remove(0);
+        // Assert
+        Assert.Equal(3, circularBuffer.Pop());
+        Assert.Equal(34, circularBuffer.Pop());
+        Assert.Equal(24, circularBuffer.Pop());
+        Assert.Equal(31, circularBuffer.Pop());
+        Assert.Equal(14, circularBuffer.Pop());
+    }
+
+    [Fact]
+    public void WhenRemoveNumbers_ThenRemoveAllTargetNumbers()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(5, false)
+        {
+            3,
+            3,
+            3,
+            31,
+            14
+        };
+        // Act
+        circularBuffer.Remove(3);
+        // Arrange
+        Assert.Equal(5 - 3, circularBuffer.Count);
+    }
+
+    [Fact]
+    public void WhenRemoveAllItems_ThenCircularBufferIsEmpty()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(3, false)
+        {
+            1,
+            2,
+            3
+        };
+        // Act
+        circularBuffer.Remove(3);
+        circularBuffer.Remove(2);
+        circularBuffer.Remove(1);
+        // Assert
+        Assert.Empty(circularBuffer);
+    }
+
+    [Fact]
+    public void WhenRemoveNumbers_ThenRemoveAllTargetNumbersAndPopSequentially()
+    {
+        // Arrange
+        var circularBuffer = new CircularBuffer<byte>(12, false)
+        {
+            1,
+            0,
+            2,
+            0,
+            3,
+            0,
+            4,
+            0,
+            5
+        };
+        // Act
+        circularBuffer.Remove(0);
+        // Assert
+        Assert.Equal(5, circularBuffer.Count);
+        Assert.Equal(1, circularBuffer.Pop());
+        Assert.Equal(2, circularBuffer.Pop());
+        Assert.Equal(3, circularBuffer.Pop());
+        Assert.Equal(4, circularBuffer.Pop());
+        Assert.Equal(5, circularBuffer.Pop());
+    }
+
+    [Fact]
+    public void WhenRemoveDuplicateStringsValues_ThenRemoveAllTargetItem()
+    {
+        // Arrange
+        var stringBuffer = new CircularBuffer<string?>(10, false)
+        {
+            "one",
+            null,
+            "two",
+            null,
+            "three",
+            null,
+            "four",
+            null,
+            "five"
+        };
+        // Act
+        stringBuffer.Remove(null);
+        // Assert        
+        Assert.Equal(5, stringBuffer.Count);
+        Assert.Equal("one", stringBuffer.Pop());
+        Assert.Equal("two", stringBuffer.Pop());
+        Assert.Equal("three", stringBuffer.Pop());
+        Assert.Equal("four", stringBuffer.Pop());
+        Assert.Equal("five", stringBuffer.Pop());
+    }
+}
---
+++ Collections/DataStructure/Lists/CircularBuffer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections.DataStructure.Lists;
+
+/// <summary>
+/// A single, fixed-size buffer as if it were connected end-to-end.
+/// </summary>
+/// <see href="https://en.wikipedia.org/wiki/Circular_buffer" />
+/// <remarks>
+/// Circular buffer. Given a fixed size, fills to capacity and then overwrites earliest item.
+/// Microsoft.VisualStudio.Utilities.CircularBuffer.
+/// </remarks>
+public class CircularBuffer<T> : ICollection<T> where T : IComparable<T>
+{
+    private readonly T[] _buffer;
+    private int _end;
+    private int _start;
+    private int _count;
+    private const int DefaultBufferLength = 10;
+
+    public int Capacity => _buffer.Length - 1;
+    public int Count => _count;
+    public bool IsReadOnly => false;
+    public bool IsEmpty => _count == 0;
+
+    public bool IsFull =>
+        (_end + 1) % _buffer.Length == _start &&
+        !_buffer[_start].Equals(_buffer[_end]);
+
+    public bool CanOverride { get; }
+
+    /// <summary>
+    /// Initializes a circular buffer with initial length of 10
+    /// </summary>
+    public CircularBuffer(bool canOverride = true) : this(DefaultBufferLength, canOverride)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a circular buffer with given length
+    /// </summary>
+    /// <param name="length">The length of the buffer</param>
+    /// <param name="canOverride"></param>
+    public CircularBuffer(int length, bool canOverride = true)
+    {
+        if (length < 1)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        _buffer = new T[length + 1];
+        _end = 0;
+        _start = 0;
+        CanOverride = canOverride;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var i = _start; i < Count; i++) yield return _buffer[i];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public T this[int index]
+    {
+        get => _buffer[InternalIndex(index)];
+        set => _buffer[InternalIndex(index)] = value;
+    }
+
+    private int InternalIndex(int index)
+    {
+        if (IsEmpty)
+            throw new IndexOutOfRangeException($"Cannot access index {index}. Buffer is empty");
+        if (index >= Count)
+            throw new IndexOutOfRangeException(
+                $"Cannot access index {index}. Buffer size is {Count}");
+        return (_start + index) % Capacity;
+    }
+
+    public void Add(T item)
+    {
+        if (CanOverride == false && IsFull)
+            throw new CircularBufferFullException();
+
+        _buffer[_end] = item;
+        _end = (_end + 1) % _buffer.Length;
+        if (_end == _start) _start = (_start + 1) % _buffer.Length;
+
+        // Count should not be greater than the length of the buffer when overriding 
+        _count = _count < Capacity ? ++_count : _count;
+    }
+
+    public T Pop()
+    {
+        var result = _buffer[_start];
+        _buffer[_start] = _buffer[_end];
+        _start = (_start + 1) % _buffer.Length;
+        //Count should not go below Zero when popping an empty buffer.
+        _count = _count > 0 ? --_count : _count;
+        return result;
+    }
+
+    public T[] ToArray()
+    {
+        if (IsEmpty) return [];
+        var array = new T[Count];
+        for (var index = 0; index < Count; ++index)
+            array[index] = this[index];
+
+        return array;
+    }
+
+    public void Clear()
+    {
+        _count = 0;
+        _start = 0;
+        _end = 0;
+        Array.Clear(_buffer, 0, _buffer.Length);
+    }
+
+    public bool Contains(T item)
+    {
+        return _buffer.Contains(item);
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        if (array == null) throw new ArgumentNullException(nameof(array));
+
+        if (array.Length == 0 || arrayIndex >= array.Length || arrayIndex < 0)
+            throw new IndexOutOfRangeException();
+
+        // Get enumerator
+        using var enumarator = GetEnumerator();
+
+        // Copy elements if there is any in the buffer and if the index is within the valid range
+        while (arrayIndex < array.Length)
+            if (enumarator.MoveNext())
+            {
+                array[arrayIndex] = enumarator.Current;
+                arrayIndex++;
+            }
+            else
+            {
+                break;
+            }
+    }
+
+    public bool Remove(T item)
+    {
+        if (IsEmpty || !Contains(item)) return false;
+        var sourceArray = _buffer.Except([item]).ToArray();
+        Clear();
+        Array.Copy(sourceArray, _buffer, sourceArray.Length);
+
+        if (!Equals(item, default(T)))
+        {
+            _end = sourceArray.Length - 1;
+            _count = sourceArray.Length - 1;
+        }
+        else
+        {
+            _end = sourceArray.Length;
+            _count = sourceArray.Length;
+        }
+
+        return true;
+    }
+}
+
+// TODO: Move out and Multilingual.
+public class CircularBufferFullException : Exception
+{
+    public CircularBufferFullException() : base(
+        "Circular Buffer is filled up. Can not be added to the circular buffer")
+    {
+    }
+
+    public CircularBufferFullException(string message) : base(message)
+    {
+    }
+
+    public CircularBufferFullException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
